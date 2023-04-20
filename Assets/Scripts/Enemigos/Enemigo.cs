4d6342734066c7fd426e787c4c8fff40d1a24c73
GER <valkar@hotmail.es>
2023-04-21 01:35:38 +0200

    Initial commit

---
+++ Assets/Scripts/Enemigos/Enemigo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public Atributos atributos;
+    public string nombre;
+    public int experiencia;
+}
